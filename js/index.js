110b09bac17b83f6dc718a90233d20dab4a6aba7
patriciamont <fiorellamonteverde@gmail.com>
2018-09-02 12:26:05 -0500

    añadiendo funcionalidad e imágenes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const btnTienda = document.getElementById('tienda')
-
+const home=document.getElementById('home')
 const containerInicio = document.getElementById('containerInicio') 
-const bebesyniños= document.getElementById('bebesyniños')
-const mamas=document.getElementById('mamas')
+const bebesyniños= document.querySelector('.containerBebesyniños')
+const mamas=document.querySelector('.containerMamas')
 const filtros=document.getElementById('filtros')
 
 btnTienda.addEventListener('click', () => {
@@ -11,4 +11,11 @@ btnTienda.addEventListener('click', () => {
 })
 bebesyniños.addEventListener('click',()=>{
 ocultar2()
+})
+
+home.addEventListener('click', ()=>{
+  containerInicio.style = 'display:block';
+  bebesyniños.style = 'display:none';
+    mamas.style = 'display:none';
+    filtros.style='display:none';
 })
