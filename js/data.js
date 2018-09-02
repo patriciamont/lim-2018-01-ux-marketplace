eb8f608d25479b4d5b272b091963d194f80fdba3
patriciamont <fiorellamonteverde@gmail.com>
2018-09-01 20:07:15 -0500

    añadiendo opciones y select para seleccionar productos

---
+++ js/data.js
@@ -0,0 +1,11 @@
+window.ocultar = () => {
+    containerInicio.style = 'display:none';
+    bebesyniños.style = 'display:block';
+    mamas.style = 'display:block'
+}
+window.ocultar2 = () => {
+    mamas.style = 'display:none'
+    filtros.style='display:block'
+
+
+}
---
+++ js/index.js
@@ -0,0 +1,14 @@
+const btnTienda = document.getElementById('tienda')
+
+const containerInicio = document.getElementById('containerInicio') 
+const bebesyniños= document.getElementById('bebesyniños')
+const mamas=document.getElementById('mamas')
+const filtros=document.getElementById('filtros')
+
+btnTienda.addEventListener('click', () => {
+  ocultar() 
+   /*  document.getElementById('containerInicio').style = 'display:none'; */
+})
+bebesyniños.addEventListener('click',()=>{
+ocultar2()
+})
